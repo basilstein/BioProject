49eda3a6f33f613c6f5c3ef1802823acecea3ada
basilstein <44881303+basilstein@users.noreply.github.com>
2020-04-21 12:09:59 -0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -21,4 +21,22 @@ for (var i = 0; i < dnastrand.length; i++) {
   }
 }
 localStorage.setItem("mrnastrand",mrnastrand);
-
+for (var i = 0; i < dnastrand.length; i = i+3) {
+  let codon = mrnastrand[i]+mrnastrand[i+1]+mrnastrand[i+2];
+  switch(codon){
+    case "a":
+      mrnastrand=mrnastrand+"u";
+      break;
+    case "t":
+      mrnastrand=mrnastrand+"a";
+      break;
+    case "c":
+      mrnastrand=mrnastrand+"g";
+      break;
+    case "g":
+      mrnastrand=mrnastrand+"c";
+      break;
+    default:
+      mrnastrand=mrnastrand+"X";
+  }
+}
