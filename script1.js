c9abf1b771658d5d46860b6a9580811e7de96fe3
basilstein <44881303+basilstein@users.noreply.github.com>
2020-04-21 12:02:37 -0700

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,21 @@
+let tmpstrand = prompt("Write 15 DNA bases");
+let dnastrand = strand.toLowerCase();
+localStorage.setItem("dnastrand",dnastrand);
+var mrnastrand;
+for (var i = 0; i < dnastrand.length; i++) {
+  switch(dnastrand[i]){
+    case "a":
+      mrnastrand=mrnastrand+"u";
+      break;
+    case "t":
+      mrnastrand=mrnastrand+"a";
+      break;
+    case "c":
+      mrnastrand=mrnastrand+"g";
+      break;
+    case "g":
+      mrnastrand=mrnastrand+"c";
+      break;
+  }
+}
+localStorage.setItem("mrnastrand",mrnastrand);
